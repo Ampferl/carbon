13bec5a4f3413db04dc2334b08fefc414076c75b
Ampferl <Jonas@ampferl.com>
2020-04-26 01:00:38 +0200

    Add token header file

---
+++ src/core/token.h
@@ -0,0 +1,16 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+#define TT_INT		"INT"
+#define TT_FLOAT	"FLOAT"
+#define TT_STRING	"STRING"
+#define TT_IDENTIFIER	"IDENTIFIER"
+#define TT_KEYWORD	"KEYWORD"
+#define TT_PLUS		"PLUS"
+#define TT_MINUS	"MINUS"
+#define TT_MUL		"MUL"
+#define TT_DIV		"DIV"
+#define TT_EQUAL	"EQ"
+
+
+#endif
