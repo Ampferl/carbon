4776c41c0852c2bef606b3bf8a8402dfc64499fd
Ampferl <Jonas@ampferl.com>
2020-05-04 19:59:13 +0200

    Added error header file

---
+++ src/core/error.h
@@ -0,0 +1,33 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "enum.h"
+#include "lex.h"
+
+
+typedef struct ErrorInfor{
+    ErrorType type;
+
+    unsigned int row;
+    unsigned int column;
+
+    char *data;
+
+} ErrorInfor;
+
+void error_throwError(ErrorType type,Token *token);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
