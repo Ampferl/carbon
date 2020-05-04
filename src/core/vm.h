302c5d7d65b8fcf96e8c2474ced07d47180922fb
Ampferl <Jonas@ampferl.com>
2020-05-04 20:04:32 +0200

    Added vm header file

---
+++ src/core/vm.h
@@ -0,0 +1,22 @@
+#ifndef VM_H
+#define VM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+#include "enum.h"
+#include "parse.h"
+
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
