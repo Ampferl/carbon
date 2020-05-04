f6efdf25dbe4d8e72d3a950441074cd1508c92ce
Ampferl <Jonas@ampferl.com>
2020-05-04 17:06:32 +0200

    Added enum header files

---
+++ src/core/enum.h
@@ -0,0 +1,22 @@
+#ifndef ENUM_H
+#define ENUM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "errorEnum.h"
+#include "lexerEnum.h"
+#include "parseEnum.h"
+#include "dataEnum.h"
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
